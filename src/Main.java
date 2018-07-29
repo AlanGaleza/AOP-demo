772c1605e2672c48628c24e7be7cd56170f3addd
“AlanGaleza <alano.gal@gmail.com>
2018-07-29 10:48:49 +0200

    @Before

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import com.aop.config.Config;
+import com.aop.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        theAccountDAO.addAccount();
+
+        context.close();
     }
 }
