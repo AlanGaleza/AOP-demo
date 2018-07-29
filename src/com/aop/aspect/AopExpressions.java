0ce671363aa597072760f16d40cc5052552418a4
“AlanGaleza <alano.gal@gmail.com>
2018-07-29 13:51:45 +0200

    ordering aspects)

---
+++ src/com/aop/aspect/AopExpressions.java
@@ -0,0 +1,20 @@
+package com.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class AopExpressions {
+
+    @Pointcut("execution(* com.aop.dao.*.*(..))")
+    public void forDaoPackage(){}
+
+    @Pointcut("execution(* com.aop.dao.*.get*(..))")
+    public void getter(){}
+
+    @Pointcut("execution(* com.aop.dao.*.set*(..))")
+    public void setter(){}
+
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    public void forDaoPackageNoGetterSetter(){}
+}
